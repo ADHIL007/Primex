ae82bdef4806443097221d99e6703e9dd91131ee
ADHIL007 <adhilshaht111@gmail.com>
2024-02-23 20:38:24 +0530

    rollback

--- components/TableGen.tsx
+++ components/TableGen.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import store from '../Redux/Store';
-import CircularProgress from 'react-native-circular-progress-indicator';
+
 const TableGen = () => {
 const data = store.getState().predData
 const school = store.getState().CurrentSchoolData
@@ -10,13 +10,7 @@ console.log(school);
 
   return (
     <View style={styles.container}>
-<CircularProgress
-  value={85}
-  inActiveStrokeColor={'#2ecc71'}
-  inActiveStrokeOpacity={0.2}
-  progressValueColor={'#fff'}
-  valueSuffix={'%'}
-/>
+
 
     </View>
   );
